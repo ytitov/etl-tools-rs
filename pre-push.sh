5f986e8c8fcc2074aa84e509a035a7476ec5561c
Yuri Titov <ytitov@kpininja.com>
2021-08-27 10:43:39 -0500

    remove some docs

--- pre-push.sh
+++ pre-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Generating documentation"
-cargo doc --no-deps --workspace --release --target-dir docs
+cargo doc --no-deps --workspace --release --target-dir docs --package etl_core --package etl_mysql --package etl_s3
 cp docs_index.html ./docs/index.html
 git add docs/
 git commit -m "Update documentation ($(date +%F@%R))"
