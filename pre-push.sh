63b91bbda6e60a7ec4fa20ffa430186dd9f527c4
Yuri Titov <ytitov@kpininja.com>
2021-08-27 10:25:22 -0500

    testing index

--- pre-push.sh
+++ pre-push.sh
@@ -2,5 +2,6 @@
 
 echo "Generating documentation"
 cargo doc --no-deps --workspace --release --target-dir docs
+cp docs_index.html ./docs/index.html
 git add docs/
 git commit -m "Update documentation ($(date +%F@%R))"
