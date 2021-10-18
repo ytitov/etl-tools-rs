1239e4cd594dc9ff3e81e427522919aba79f684a
Yuri Titov <ytitov@gmail.com>
2021-10-18 12:37:09 -0500

    add s3 file listing

--- etl-s3/src/lib.rs
+++ etl-s3/src/lib.rs
@@ -1,10 +1,51 @@
 use etl_core::preamble::*;
+use regex::Regex;
 use rusoto_core::HttpClient;
 use rusoto_credential::ProfileProvider;
+pub use rusoto_credential::{ChainProvider, ProvideAwsCredentials};
 use rusoto_s3::*;
 use std::collections::HashSet;
 
 pub mod datastore;
+
+pub struct ListS3FilesRequest<P: ProvideAwsCredentials + Send + Sync + 'static> {
+    pub profile_provider: P,
+    pub bucket: String,
+    pub prefix: Option<String>,
+    pub regex: Option<Regex>,
+    pub region: rusoto_core::Region,
+}
+
+pub async fn list_s3_files<P: ProvideAwsCredentials + Send + Sync + 'static>(
+    req: ListS3FilesRequest<P>,
+) -> anyhow::Result<Vec<String>> {
+    let mut r = Vec::new();
+    let client = S3Client::new_with(HttpClient::new()?, req.profile_provider, req.region);
+    let request = ListObjectsV2Request {
+        bucket: req.bucket.clone(),
+        prefix: req.prefix.clone(),
+        ..Default::default()
+    };
+    let mut result = client.list_objects_v2(request).await?;
+    fill_keys(&mut r, result.contents);
+    while result.is_truncated.is_some() && result.is_truncated.unwrap() == true {
+        result = client
+            .list_objects_v2(ListObjectsV2Request {
+                bucket: req.bucket.clone(),
+                prefix: req.prefix.clone(),
+                continuation_token: result.next_continuation_token,
+                ..Default::default()
+            })
+            .await?;
+        fill_keys(&mut r, result.contents);
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
+    if let Some(re_exp) = req.regex {
+        Ok(r.into_iter().filter(|n| re_exp.is_match(&n)).collect())
+    } else {
+        Ok(r)
+    }
+}
 
 /// lists all s3 files and keeps making the requests until is_truncated is false
 pub async fn list_s3_files_custom(
@@ -53,7 +94,10 @@ pub async fn is_result_on_s3(
 ) -> anyhow::Result<bool> {
     let p = ProfileProvider::with_default_configuration(s3_credentials);
     let (_, files) = list_s3_files_custom(p, s3_bucket, Some(s3_bucket_key_prefix), None).await?;
-    let path = format!("{}{}{}", s3_bucket_key_prefix, client_request_token, file_extension);
+    let path = format!(
+        "{}{}{}",
+        s3_bucket_key_prefix, client_request_token, file_extension
+    );
     //println!("Looking for path: {}", &path);
     for file in files {
         if file == path {
