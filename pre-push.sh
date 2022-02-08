5ae327fef290a99e6ac08a399ebfbd4cd6ad6d02
Yuri Titov <ytitov@kpininja.com>
2022-02-08 15:39:08 -0600

    update script

--- pre-push.sh
+++ pre-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Generating documentation"
-cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-s3
+cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp
 cp docs_index.html ./docs/index.html
 git add docs/
 git commit -m "Update documentation ($(date +%F@%R))"
