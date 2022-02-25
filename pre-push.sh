b888f891e8806adec2b749f0e1d3f9bd81945973
Yuri Titov <ytitov@kpininja.com>
2022-02-25 13:48:16 -0600

    info on adding hook

--- pre-push.sh
+++ pre-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# to add this hook run the following command: ln -s -f ../../pre-push.sh .git/hooks/pre-push
 echo "Generating documentation"
 cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp
 cp docs_index.html ./docs/index.html
