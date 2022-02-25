db59690667feb8a7e60668652466d380620db268
Yuri Titov <ytitov@kpininja.com>
2022-02-25 13:56:38 -0600

    remove auto commit

--- pre-push.sh
+++ pre-push.sh
@@ -3,5 +3,5 @@
 echo "Generating documentation"
 cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp
 cp docs_index.html ./docs/index.html
-git add docs/
-git commit -m "Update documentation ($(date +%F@%R))"
+#git add docs/
+#git commit -m "Update documentation ($(date +%F@%R))"
