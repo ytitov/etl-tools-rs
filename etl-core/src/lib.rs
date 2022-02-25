70da04043e2527cf8db4d99bbdc0f288b5124bdf
Yuri Titov <ytitov@kpininja.com>
2022-02-25 14:23:41 -0600

    clarify the main documentation

--- etl-core/src/lib.rs
+++ etl-core/src/lib.rs
@@ -1,3 +1,9 @@
+//! This library provides a foundation to construct pipelines using several basic principles.
+//! There are stream producers and consumers, which must implement the traits [crate::datastore::DataSource]
+//! and [crate::datastore::DataOutput].  [crate::job::JobRunner] is used to construct pipelines 
+//! using these elements.  It also provides some simple state management which is loaded and
+//! saved using the [crate::datastore::SimpleStore] trait which is designed for lading whole
+//! files
 pub mod datastore;
 /// Used by the [crate::datastore::bytes_source::BytesSource] to help decode various streams.  So
 /// anything that implements a BytesSource target, can select which decoder they want to use.  For
