adfab36b6bb6fbc6f43293492adcec75b53e7d04
Yuri Titov <ytitov@kpininja.com>
2022-03-09 10:42:06 -0600

    add etl-job

--- pre-commit-build-docs.sh
+++ pre-commit-build-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # add the hook: ln -s -f ../../pre-commit-build-docs.sh .git/hooks/pre-commit
 echo "Generating documentation automatically, you may need to add some things to the commit"
-cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp
+cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp --package etl-job
 cp docs_index.html ./docs/index.html
 #git add docs/
 #git commit -m "Update documentation ($(date +%F@%R))"
