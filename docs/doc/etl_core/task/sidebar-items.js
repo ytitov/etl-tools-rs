1721d67d9e25fe7570612b90a7ffcb4fa755c2cd
Yuri Titov <ytitov@kpininja.com>
2022-04-20 15:03:36 -0500

    update the docs

--- docs/doc/etl_core/task/sidebar-items.js
+++ docs/doc/etl_core/task/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["apply","accept a DataSource and execute an async function with a custom state"]]});
+initSidebarItems({"mod":[["apply","accept a DataSource and execute an async function with a custom state"],["pipeline","Takes a DataSource and a DataOutput and executes them as a task"]]});
