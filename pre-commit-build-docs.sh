fa0297b9a6029eab3b606216dd2823517c210edd
Yuri Titov <ytitov@gmail.com>
2022-05-04 23:18:57 -0500

    ensure only runs on master

--- pre-commit-build-docs.sh
+++ pre-commit-build-docs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # add the hook: ln -s -f ../../pre-commit-build-docs.sh .git/hooks/pre-commit
-echo "Generating documentation automatically, you may need to add some things to the commit"
-cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp
-cp docs_index.html ./docs/index.html
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ $BRANCH == "master" ]; then
+  echo "Generating documentation automatically, you may need to add some things to the commit"
+  cargo doc --no-deps --release --target-dir docs --package etl-core --package etl-mysql --package etl-aws-utils --package etl-sftp
+  cp docs_index.html ./docs/index.html
+fi
+
+
 #git add docs/
 #git commit -m "Update documentation ($(date +%F@%R))"
